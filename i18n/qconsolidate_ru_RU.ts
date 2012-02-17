dc6f08334be0d9d145572c0bf8c298b4d2aab36c
Alexander Bruy <alexander.bruy@gmail.com>
2012-02-17 20:13:05 +0200

    add .ts files for Russian and Ukrainian languages

---
+++ i18n/qconsolidate_ru_RU.ts
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>QConsolidate</name>
+    <message>
+        <location filename="qconsolidate.py" line="74"/>
+        <source>Quantum GIS version detected: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="74"/>
+        <source>This version of QConsolidate requires at least QGIS version 1.0.0
+Plugin will not be enabled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="80"/>
+        <source>Moves overlapped points with same coordinates in a circle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="92"/>
+        <source>QConsolidate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="97"/>
+        <source>About QConsolidate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="99"/>
+        <source>&lt;b&gt;QConsolidate&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="102"/>
+        <source>Version: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="105"/>
+        <source>Consolidate QGIS project into one directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="106"/>
+        <source>&lt;b&gt;Developers:&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="108"/>
+        <source>&lt;b&gt;Homepage:&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="125"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QConsolidateDialog</name>
+    <message>
+        <location filename="qconsolidatedialog.py" line="105"/>
+        <source>QConsolidate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidatedialogbase.ui" line="20"/>
+        <source>Output directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidatedialogbase.ui" line="32"/>
+        <source>Browse...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidatedialog.py" line="72"/>
+        <source>Select output directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidatedialog.py" line="84"/>
+        <source>QConsolidate: Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidatedialog.py" line="84"/>
+        <source>Can&apos;t create directory for layers.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidatedialog.py" line="105"/>
+        <source>Completed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidatedialog.py" line="144"/>
+        <source>Project loagind error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidatedialog.py" line="132"/>
+        <source>Cannot read file %1:
+%2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidatedialog.py" line="144"/>
+        <source>Parse error at line %1, column %2:
+%3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidatedialog.py" line="175"/>
+        <source>Project saving error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidatedialog.py" line="175"/>
+        <source>Cannot write file %1:
+%2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
