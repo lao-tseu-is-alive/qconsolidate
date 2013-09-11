394f5e6f84c2017452980e5a2b5e9c6b77661aef
Alexander Bruy <alexander.bruy@gmail.com>
2013-09-11 17:10:25 +0300

    strings update

--- i18n/qconsolidate_ru.ts
+++ i18n/qconsolidate_ru.ts
@@ -1,33 +1,103 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<!DOCTYPE TS><TS version="2.0" language="ru_RU" sourcelanguage="">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="aboutdialog.py" line="54"/>
+        <source>Version: %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.py" line="79"/>
+        <source>&lt;p&gt;Consolidates all layers from current QGIS project into one directory.&lt;/p&gt;
+    &lt;p&gt;&lt;strong&gt;Developers&lt;/strong&gt;: Alexander Bruy&lt;/p&gt;
+    &lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;http://hub.qgis.org/projects/qconsolidate&quot;&gt;http://hub.qgis.org/projects/qconsolidate&lt;/a&gt;&lt;/p&gt;
+    &lt;p&gt;Please report bugs at &lt;a href=&quot;http://hub.qgis.org/projects/qconsolidate/issues&quot;&gt;bugtracker&lt;/a&gt;.&lt;/p&gt;
+    </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 <context>
     <name>ConsolidateThread</name>
     <message>
         <location filename="consolidatethread.py" line="159"/>
         <source>Cannot read file %1:
 %2.</source>
-        <translation>Не удалось прочитать файл %1:
+        <translation type="obsolete">Не удалось прочитать файл %1:
 %2.</translation>
     </message>
     <message>
         <location filename="consolidatethread.py" line="166"/>
         <source>Parse error at line %1, column %2:
 %3</source>
-        <translation>Ошибка разбора в строке %1, позиция %2:
+        <translation type="obsolete">Ошибка разбора в строке %1, позиция %2:
 %3</translation>
     </message>
     <message>
         <location filename="consolidatethread.py" line="176"/>
         <source>Cannot write file %1:
 %2.</source>
-        <translation>Не удалось записать файл %1:
+        <translation type="obsolete">Не удалось записать файл %1:
 %2.</translation>
     </message>
     <message>
         <location filename="consolidatethread.py" line="207"/>
         <source>Cannot copy layer %1</source>
-        <translation>Не удалось скопировать слой %1</translation>
+        <translation type="obsolete">Не удалось скопировать слой %1</translation>
+    </message>
+    <message>
+        <location filename="consolidatethread.py" line="171"/>
+        <source>Cannot read file %s:
+%s.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="consolidatethread.py" line="178"/>
+        <source>Parse error at line %d, column %d:
+%s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="consolidatethread.py" line="188"/>
+        <source>Cannot write file %s:
+%s.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="consolidatethread.py" line="219"/>
+        <source>Cannot copy layer %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="14"/>
+        <source>About QConsolidate</source>
+        <translation type="unfinished">О QConsolidate</translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="44"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="30"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:16pt; font-weight:600;&quot;&gt;QConsolidate&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="67"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -35,52 +105,52 @@
     <message>
         <location filename="qconsolidate.py" line="74"/>
         <source>Quantum GIS version detected: </source>
-        <translation>Обнаружена QGIS версии: </translation>
+        <translation type="obsolete">Обнаружена QGIS версии: </translation>
     </message>
     <message>
-        <location filename="qconsolidate.py" line="92"/>
+        <location filename="qconsolidate.py" line="90"/>
         <source>QConsolidate</source>
         <translation>QConsolidate</translation>
     </message>
     <message>
         <location filename="qconsolidate.py" line="97"/>
         <source>About QConsolidate</source>
-        <translation>О QConsolidate</translation>
+        <translation type="obsolete">О QConsolidate</translation>
     </message>
     <message>
         <location filename="qconsolidate.py" line="99"/>
         <source>&lt;b&gt;QConsolidate&lt;/b&gt;</source>
-        <translation>&lt;b&gt;QConsolidate&lt;/b&gt;</translation>
+        <translation type="obsolete">&lt;b&gt;QConsolidate&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="qconsolidate.py" line="102"/>
         <source>Version: %1</source>
-        <translation>Версия: %1</translation>
+        <translation type="obsolete">Версия: %1</translation>
     </message>
     <message>
         <location filename="qconsolidate.py" line="106"/>
         <source>&lt;b&gt;Developers:&lt;/b&gt;</source>
-        <translation>&lt;b&gt;Разработчики:&lt;/b&gt;</translation>
+        <translation type="obsolete">&lt;b&gt;Разработчики:&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="qconsolidate.py" line="108"/>
         <source>&lt;b&gt;Homepage:&lt;/b&gt;</source>
-        <translation>&lt;b&gt;Домашняя страница:&lt;/b&gt;</translation>
+        <translation type="obsolete">&lt;b&gt;Домашняя страница:&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="qconsolidate.py" line="125"/>
         <source>Close</source>
-        <translation>Закрыть</translation>
+        <translation type="obsolete">Закрыть</translation>
     </message>
     <message>
         <location filename="qconsolidate.py" line="74"/>
         <source>This version of QConsolidate requires at least QGIS version 1.8.0
 Plugin will not be enabled.</source>
-        <translation>Эта версия QConsolidate требует QGIS 1.8.0 или старше
+        <translation type="obsolete">Эта версия QConsolidate требует QGIS 1.8.0 или старше
 Модуль не будет загружен.</translation>
     </message>
     <message>
-        <location filename="qconsolidate.py" line="80"/>
+        <location filename="qconsolidate.py" line="78"/>
         <source>Consolidates all layers from current QGIS project into one directory</source>
         <translation>Собирает все слои открытого проекта QGIS в одном каталоге</translation>
     </message>
@@ -88,63 +158,75 @@ Plugin will not be enabled.</source>
         <location filename="qconsolidate.py" line="105"/>
         <source>Consolidates all layers from current QGIS project
 into one directory</source>
-        <translation>Собирает все слои открытого проекта QGIS в одном каталоге</translation>
+        <translation type="obsolete">Собирает все слои открытого проекта QGIS в одном каталоге</translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="72"/>
+        <source>QGIS %s detected.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="72"/>
+        <source>This version of QConsolidate requires at least QGIS version 2.0.
+Plugin will not be enabled.</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>QConsolidateDialog</name>
     <message>
-        <location filename="qconsolidatedialogbase.ui" line="14"/>
+        <location filename="ui/qconsolidatedialogbase.ui" line="14"/>
         <source>QConsolidate</source>
         <translation>QConsolidate</translation>
     </message>
     <message>
-        <location filename="qconsolidatedialogbase.ui" line="20"/>
+        <location filename="ui/qconsolidatedialogbase.ui" line="20"/>
         <source>Output directory</source>
         <translation>Каталог назначения</translation>
     </message>
     <message>
-        <location filename="qconsolidatedialogbase.ui" line="32"/>
+        <location filename="ui/qconsolidatedialogbase.ui" line="32"/>
         <source>Browse...</source>
         <translation>Обзор...</translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="53"/>
+        <location filename="qconsolidatedialog.py" line="55"/>
         <source>Select output directory</source>
         <translation>Выберите каталог назначения</translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="121"/>
+        <location filename="qconsolidatedialog.py" line="130"/>
         <source>QConsolidate: Error</source>
         <translation>QConsolidate: Ошибка</translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="64"/>
+        <location filename="qconsolidatedialog.py" line="67"/>
         <source>Output directory is not set. Please specify output directory.</source>
         <translation>Не задан каталог назначения. Пожалуйста укажите его.</translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="72"/>
+        <location filename="qconsolidatedialog.py" line="76"/>
         <source>Directory exists</source>
         <translation>Каталог существует</translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="72"/>
+        <location filename="qconsolidatedialog.py" line="76"/>
         <source>Output directory already contains &apos;layers&apos; subdirectory. Maybe this directory was used to consolidate another project. Continue?</source>
         <translation>Каталог назначения уже содержит вложенный каталог &apos;layers&apos;. Возможно, этот каталог уже использовался для консолидации другого проекта. Продолжить?</translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="81"/>
+        <location filename="qconsolidatedialog.py" line="86"/>
         <source>Can&apos;t create directory for layers.</source>
         <translation>Не удалось создать каталог для слоёв.</translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="100"/>
+        <location filename="qconsolidatedialog.py" line="106"/>
         <source>Cancel</source>
         <translation>Отмена</translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="138"/>
+        <location filename="qconsolidatedialog.py" line="148"/>
         <source>Close</source>
         <translation>Закрыть</translation>
     </message>
--- i18n/qconsolidate_uk.ts
+++ i18n/qconsolidate_uk.ts
@@ -1,145 +1,158 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="aboutdialog.py" line="54"/>
+        <source>Version: %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.py" line="79"/>
+        <source>&lt;p&gt;Consolidates all layers from current QGIS project into one directory.&lt;/p&gt;
+    &lt;p&gt;&lt;strong&gt;Developers&lt;/strong&gt;: Alexander Bruy&lt;/p&gt;
+    &lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;http://hub.qgis.org/projects/qconsolidate&quot;&gt;http://hub.qgis.org/projects/qconsolidate&lt;/a&gt;&lt;/p&gt;
+    &lt;p&gt;Please report bugs at &lt;a href=&quot;http://hub.qgis.org/projects/qconsolidate/issues&quot;&gt;bugtracker&lt;/a&gt;.&lt;/p&gt;
+    </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 <context>
     <name>ConsolidateThread</name>
     <message>
-        <location filename="consolidatethread.py" line="159"/>
-        <source>Cannot read file %1:
-%2.</source>
+        <location filename="consolidatethread.py" line="171"/>
+        <source>Cannot read file %s:
+%s.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="consolidatethread.py" line="166"/>
-        <source>Parse error at line %1, column %2:
-%3</source>
+        <location filename="consolidatethread.py" line="178"/>
+        <source>Parse error at line %d, column %d:
+%s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="consolidatethread.py" line="176"/>
-        <source>Cannot write file %1:
-%2.</source>
+        <location filename="consolidatethread.py" line="188"/>
+        <source>Cannot write file %s:
+%s.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="consolidatethread.py" line="207"/>
-        <source>Cannot copy layer %1</source>
+        <location filename="consolidatethread.py" line="219"/>
+        <source>Cannot copy layer %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="14"/>
+        <source>About QConsolidate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="44"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="30"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:16pt; font-weight:600;&quot;&gt;QConsolidate&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="67"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>QConsolidate</name>
     <message>
-        <location filename="qconsolidate.py" line="74"/>
-        <source>Quantum GIS version detected: </source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidate.py" line="92"/>
+        <location filename="qconsolidate.py" line="90"/>
         <source>QConsolidate</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidate.py" line="97"/>
-        <source>About QConsolidate</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidate.py" line="99"/>
-        <source>&lt;b&gt;QConsolidate&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidate.py" line="102"/>
-        <source>Version: %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidate.py" line="106"/>
-        <source>&lt;b&gt;Developers:&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidate.py" line="108"/>
-        <source>&lt;b&gt;Homepage:&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidate.py" line="125"/>
-        <source>Close</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidate.py" line="74"/>
-        <source>This version of QConsolidate requires at least QGIS version 1.8.0
-Plugin will not be enabled.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="qconsolidate.py" line="80"/>
+        <location filename="qconsolidate.py" line="78"/>
         <source>Consolidates all layers from current QGIS project into one directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidate.py" line="105"/>
-        <source>Consolidates all layers from current QGIS project
-into one directory</source>
+        <location filename="qconsolidate.py" line="72"/>
+        <source>QGIS %s detected.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qconsolidate.py" line="72"/>
+        <source>This version of QConsolidate requires at least QGIS version 2.0.
+Plugin will not be enabled.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>QConsolidateDialog</name>
     <message>
-        <location filename="qconsolidatedialogbase.ui" line="14"/>
+        <location filename="ui/qconsolidatedialogbase.ui" line="14"/>
         <source>QConsolidate</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialogbase.ui" line="20"/>
+        <location filename="ui/qconsolidatedialogbase.ui" line="20"/>
         <source>Output directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialogbase.ui" line="32"/>
+        <location filename="ui/qconsolidatedialogbase.ui" line="32"/>
         <source>Browse...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="53"/>
+        <location filename="qconsolidatedialog.py" line="55"/>
         <source>Select output directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="121"/>
+        <location filename="qconsolidatedialog.py" line="130"/>
         <source>QConsolidate: Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="64"/>
+        <location filename="qconsolidatedialog.py" line="67"/>
         <source>Output directory is not set. Please specify output directory.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="72"/>
+        <location filename="qconsolidatedialog.py" line="76"/>
         <source>Directory exists</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="72"/>
+        <location filename="qconsolidatedialog.py" line="76"/>
         <source>Output directory already contains &apos;layers&apos; subdirectory. Maybe this directory was used to consolidate another project. Continue?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="81"/>
+        <location filename="qconsolidatedialog.py" line="86"/>
         <source>Can&apos;t create directory for layers.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="100"/>
+        <location filename="qconsolidatedialog.py" line="106"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qconsolidatedialog.py" line="138"/>
+        <location filename="qconsolidatedialog.py" line="148"/>
         <source>Close</source>
         <translation type="unfinished"></translation>
     </message>
